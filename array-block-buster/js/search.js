f6dae2e9607a7288b1cd674e5c3fb5406a84e07b
cituk <programaciondyn3.mx@sandos.com>
2021-08-21 17:43:03 -0500

    Array.prototype.find - Buscando películas por ID

--- array-block-buster/js/search.js
+++ array-block-buster/js/search.js
@@ -6,9 +6,14 @@ const search = window['search-form']
 search.addEventListener('submit', function(event) {
     event.preventDefault()
     const formData = new FormData(this)
-    const title = formData.get('title')
-    const movies = filtersByTitle(title)
-    render( movies ) 
+    const query = formData.get('title')
+    if(query != "" ){
+        const movies = searchMovie(query) 
+        if(movies){
+            return render(movies) 
+        }
+        return alert('no se encontraron resultados')
+    } 
 })
 
 function filtersByTitle(title) {
@@ -17,8 +22,29 @@ function filtersByTitle(title) {
     })
 }
 
+function findById(id) {
+    return movies.find( (movie) => {
+        return movie.id === parseInt(id, 10) 
+    })
+}
+
+
+function searchMovie(query) {
+    
+    if( isNaN(query) ){
+        return filtersByTitle(query)
+    }
+    const movie = findById(query)
+    return movie ? [ movie ] : []
+}
+
 /*
 Array.prototype.filter - Buscando películas por nombre
 
 Crear un nuevo array con los elementos que cumplan la condición de la función dada.
+
+-----------
+Array.prototype.find - Buscando películas por ID
+
+Find te ayuda a encontrar un elemento dentro de una lista, el iterador terminará luego de encontrar la primer coincidencia.
 */
