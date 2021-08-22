6e205c00f915296388cdd2cc6b3a23a9b4cdfb16
cituk <programaciondyn3.mx@sandos.com>
2021-08-21 23:34:56 -0500

    Recreando React y React DOM

---
+++ react/components/user.js
@@ -0,0 +1,22 @@
+import { Component } from './../lib/react.js';
+
+class User extends Component {
+    render() {
+        const { avatar, name } = this.props
+        return `
+            <div class="user">
+                <div class="avatar">
+                    <img src="${avatar}" alt="">
+                </div>
+                <h2>${name}</h2>
+            </div>
+        `
+    }
+}
+
+export default User 
+
+/*
+Recreando React y React DOM
+
+Seguro has usado el método render de ReactDOM o un Component de React, en esta clase no solo los vamos a usar, antes los vamos a crear con todo lo aprendido previamente en este curso. */
